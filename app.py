1e76dda16baef62c325a49e7a68a4dd6dd93e5c0
SamiaBenNasr <samia.bennasr@insat.ucar.tn>
2025-12-08 21:57:20 +0100

    evaluating

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Analyse de Sentiment", layout="wide")
+
+st.title("🎓 Présentation des Modèles d’Analyse de Sentiments")
+
+st.markdown("""
+Bienvenue dans l’application de démonstration.
+
+👉 Utilisez le menu à gauche pour :
+- Tester le **Modèle 1**
+- Tester le **Modèle 2**
+- Comparer les deux modèles
+""")
